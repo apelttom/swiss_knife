d2241ec1b42d69bc7db339d1dbc3a4c4ed1e4698
Tomáš Apeltauer <tomas.apeltauer@external.t-mobile.cz>
2020-02-26 14:06:26 +0100

    NEW: SwissKnife program to help me survive in the world of corporate

---
+++ SwissKnife.java
@@ -0,0 +1,11 @@
+/* This is a simple Java program that helps me solving
+	problems during my work in the corporate.
+   FileName : "SwissKnife.java". */
+class SwissKnife 
+{ 
+    // Program begins with a call to main().  
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
